39940dab46596e39945080a405b4bf260d60344d
leonidasdel <55215042+leonidasdel@users.noreply.github.com>
2022-04-02 14:05:26 +0300

    add advice slip component and add main font colour style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AdviceSlipComponent } from './advice-slip/advice-slip.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdviceSlipComponent
   ],
   imports: [
     BrowserModule
